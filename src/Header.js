2307268335ac1c3e7e10848ca1f358cfda901814
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-25 21:16:11 -0400

    chore: Refactor Header component to include left and right sections

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,15 @@ function Header() {
   return (
     <div className='header'>
         <h1>This is the header</h1>
+
+        <div className='.header__left'>
+
+        </div>
+
+        <div className='.header__right'>
+
+        </div>
+
     </div>
   )
 }
