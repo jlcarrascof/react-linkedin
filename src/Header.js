aeabb018864c07d99d855bc40ac47b506d2064d1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-25 21:17:19 -0400

    chore: Update Header component to include left and right sections

--- src/Header.js
+++ src/Header.js
@@ -6,11 +6,16 @@ function Header() {
     <div className='header'>
         <h1>This is the header</h1>
 
-        <div className='.header__left'>
+        <div className='header__left'>
+            <img src='' alt='' />
+
+            <div className='header__search'>
+
+            </div>
 
         </div>
 
-        <div className='.header__right'>
+        <div className='header__right'>
 
         </div>
 
