0ddf2e7dc0775a767f08a1e7dcbc5e1c376d4025
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-25 20:38:00 -0400

    chore: Update App.js to include Header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Let's build a LinkedIn clone</h1>
+
+      {/* Header */}
+      <Header />
+
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
     </div>
-
-    /* Header */
-      /* Sidebar */
-      /* Feed */
-      /* Widgets */
-
   );
 }
 
