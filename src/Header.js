30ac0435643712b5049778d6c749da35b4df3acf
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-25 22:19:50 -0400

    chore: Update Header component to include lazy loading for images

--- src/Header.js
+++ src/Header.js
@@ -5,8 +5,6 @@ import SearchIcon from '@material-ui/icons/Search';
 function Header() {
   return (
     <div className='header'>
-        <h1>This is the header</h1>
-
         <div className='header__left'>
             <img src='https://cdn-icons-png.flaticon.com/512/174/174857.png' alt='' />
 
