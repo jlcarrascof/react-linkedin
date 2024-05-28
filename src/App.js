ae9154a71a233349498e1a6fde3c3e286f49d819
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-27 20:58:36 -0400

    chore: Update App.js to remove commented out code and add Sidebar component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Header from './Header';
 function App() {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
 
-      {/* Sidebar */}
+      {/* <Sidebar /> */}
+
       {/* Feed */}
       {/* Widgets */}
     </div>
