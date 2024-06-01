f64ad504cb3e378c6b82a7d9736da45dfa475c82
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-01 19:57:46 -0400

    Update Sidebar component to fix typo in user's name and add view statistics

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,15 +7,19 @@ function Sidebar() {
     <div className='sidebar__top'>
         <img src='' alt='' />
         <Avatar className='sidebar__avatar' />
-        <h2>Javier Martineze</h2>
+        <h2>Javier Martinez</h2>
         <h4>javier.martinez@gmail.com</h4>
     </div>
 
     <div className='sidebar__stats'>
         <div className='sidebar__stat'>
+            <p>Who viewed you</p>
+            <p className='sidebar__statNumber'>2,543</p>
         </div>
 
         <div className='sidebar__stat'>
+            <p>Views on post</p>
+            <p className='sidebar__statNumber'>2,448</p>
         </div>
     </div>
   </div>;
