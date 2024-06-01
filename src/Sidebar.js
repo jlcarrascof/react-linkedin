2f37dcb34b02cd03b3e5ed65b0fbbe858b1887a0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-01 19:43:02 -0400

    Update Sidebar component to include user information in the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Sidebar.css'
+import { Avatar } from '@material-ui/core';
 
 function Sidebar() {
   return <div className='sidebar'>
-
+    <div className='sidebar__top'>
+        <img src='' alt='' />
+        <Avatar />
+        <h2>Me</h2>
+        <h4>5h3h4@example.com</h4>
+    </div>
   </div>;
 
 }
