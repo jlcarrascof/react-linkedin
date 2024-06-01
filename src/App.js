c8889f150f685eec5254f625541368f7bc7630ea
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-01 19:25:15 -0400

    chore: Update App.js to include Sidebar component and improve layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
 
-      {/* <Sidebar /> */}
+      <div className="app__body">
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
 
-      {/* Feed */}
-      {/* Widgets */}
     </div>
   );
 }
