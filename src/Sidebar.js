4d17706e655a0706b6ab104da1996fd5f328103f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-01 21:20:32 -0400

    chore: Update Sidebar component to include user information and improve styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,7 +5,7 @@ import { Avatar } from '@material-ui/core';
 function Sidebar() {
   return <div className='sidebar'>
     <div className='sidebar__top'>
-        <img src='' alt='' />
+        <img src='https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80' alt='' />
         <Avatar className='sidebar__avatar' />
         <h2>Javier Martinez</h2>
         <h4>javier.martinez@gmail.com</h4>
